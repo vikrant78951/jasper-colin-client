c2d697b2fbc01485b0095dac296ec21829a2bc9b
vikrant78951 <vikrant78951@gmail.com>
2025-03-14 19:59:44 +0530

    spinner added

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,20 +1,15 @@
 import { axiosInstance } from "@/lib/axiosInstance";
 import { API } from "@/lib/data";
 import Wrapper from "@/components/ui/wrapper";
-
+import { Suspense } from "react";
 import ErrorContainer from "@components/ui/error";
 import ProductList from "@/components/products/product-list";
 import AddProductButton from "@/components/products/add-product-button";
-export default async function Page() {
+import Spinner from "@components/ui/spinner";
+
+export async function Component() {
   try {
-     const { data } = await axiosInstance.get(API.products, {
-       headers: {
-         "Cache-Control": "no-cache, no-store, must-revalidate",
-         Pragma: "no-cache",
-         Expires: "0",
-       },
-       params: { _t: Date.now() }, // Add timestamp to bypass cache
-     });
+    const { data } = await axiosInstance.get(API.products);
 
     if (!Array.isArray(data.data)) {
       throw new Error("Invalid API response: Expected an array");
@@ -36,4 +31,18 @@ export default async function Page() {
     console.error("Failed to fetch products:", (error as Error)?.message);
     return <ErrorContainer text={"Error fetching products"} />;
   }
+}
+
+export default async function Page() {
+  return (
+    <Suspense
+      fallback={
+        <div className="p-4 w-full flex justify-center items-center">
+          <Spinner />
+        </div>
+      }
+    >
+      <Component />
+    </Suspense>
+  );
 }
